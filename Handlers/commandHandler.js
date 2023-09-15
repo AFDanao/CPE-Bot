139cbcada5fb1f310ada17cfef7af6aa1bd096a5
AFDanao <danao.adrian@ue.edu.ph>
2023-09-15 11:43:38 +0800

    Updated Command Handler

--- Handlers/commandHandler.js
+++ Handlers/commandHandler.js
@@ -1,31 +1,43 @@
-async function loadCommands(client) {
-  const { loadFiles } = require("../Functions/fileLoader");
-  const ascii = require("ascii-table");
-  const table = new ascii("Commands").setHeading("Name", "Status");
+const { loadFiles } = require("../Functions/fileLoader");
 
-  await client.commands.clear();
-  await client.subCommands.clear();
+async function loadCommands(client) {
+  console.time("Commands Loaded");
+  // const ascii = require("ascii-table");
+  // const table = new ascii("Commands").setHeading("Name", "Status");
+
+  client.commands = new Map();
+  const commands = new Array();
+
+  client.subCommands = new Map();
+
+  // await client.commands.clear();
+  // await client.subCommands.clear();
 
   let commandsArray = [];
 
-  const Files = await loadFiles("Commands");
+  const files = await loadFiles("Commands");
 
-  Files.forEach((file) => {
-    const command = require(file);
+  for (const file of files) {
+    try {
+      const command = require(file);
 
-    if (command.subCommand)
-      return client.subCommands.set(command.subCommand, command);
+      if (command.subCommand)
+        return client.subCommands.set(command.subCommand, command);
 
-    client.commands.set(command.data.name, command);
+      client.commands.set(command.data.name, command);
 
-    commandsArray.push(command.data.toJSON());
+      commandsArray.push(command.data.toJSON());
 
-    table.addRow(command.data.name, "🟩");
-  });
-
+      commands.push({ Command: command.data.name, Status: "🟩" })
+    } catch (error) {
+      commands.push({ command: file.split("/").pop().slice(0, -3), Status: "🟥" })
+    }
+  }
   client.application.commands.set(commandsArray);
 
-  return console.log(table.toString(), "\nCommands Loaded");
+  console.table(commands, ["Command", "Status"]);
+  console.info("\n\x1b[36m%s\x1b[0m", "Loaded Commands");
+  console.timeEnd("Commands Loaded");
 }
 
 module.exports = { loadCommands };
